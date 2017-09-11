6ec17bed4ff2e6138a0ac7fa80c930b914a62333
romulo.banares <romulo88@hotmail.com>
2017-09-11 13:30:14 +0200

    add User model

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'trabtracker',
+    user: process.env.DB_USER || 'trabtracker',
+    password: process.env.DB_PASS || 'trabtracker',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './tabtracker.sqlite'
+    }
+  }
+}
