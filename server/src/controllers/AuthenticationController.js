c0472bd86997581a4700da88b8bfb56020044fbc
romulo.banares <romulo88@hotmail.com>
2017-09-12 00:21:34 +0200

    change AuthenticationController

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,21 +1,18 @@
 const router = require('express').Router()
 const {User} = require('../models')
 
-const createUser = async (user) => {
-  try {
-    const r = await User.create(user)
-    return r.toJSON()
-  } catch (err) {
-    return {
-      error: 'This email account is already in use.'
-    }
-  }
-}
-
 router.post('/', (req, res) => {
-  createUser(req.body).then((user) => {
-    res.send(user)
-  })
+  User.create(req.body)
+    .then(user => {
+      res.send(user)
+    })
+    .catch((err) => {
+      if (err) {
+        res.status(400).send({
+          error: 'This email account is already in use'
+        })
+      }
+    })
 })
 
 module.exports = router
