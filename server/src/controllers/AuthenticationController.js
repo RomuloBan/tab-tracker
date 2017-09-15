7e63d0d0210636cc6043f190409803f438385fb7
romulo.banares <romulo88@hotmail.com>
2017-09-15 11:28:40 +0200

    add password hash

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,7 +13,11 @@ module.exports = {
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
     } catch (err) {
       res.status(400).send({
         error: 'This email account is already in use'
@@ -36,16 +40,16 @@ module.exports = {
         })
       }
 
-      const isPasswordValid = password === user.password
+      const isPasswordValid = await user.comparePassword(password)
       if (!isPasswordValid) {
         return res.status(403).send({
           error: 'The login information was incorrect.'
         })
       }
-      const userJSON = user.toJSON()
+      const userJson = user.toJSON()
       res.send({
-        user: userJSON,
-        token: jwtSignUser(userJSON)
+        user: userJson,
+        token: jwtSignUser(userJson)
       })
     } catch (err) {
       res.status(500).send({
