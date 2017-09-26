383fff4704fa3e4804249195e8919b7c0ff77764
romulo.banares <romulo88@hotmail.com>
2017-09-26 12:31:03 +0200

    add folder for ViewSong component

---
+++ client/src/components/ViewSong/ViewSong.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+  <v-layout>
+    <v-flex md5>
+      <song-metadata :song="song" />
+    </v-flex>
+    <v-flex md6 offset-md1>
+      <you-tube :youtubeId="song.youtubeId" />
+    </v-flex>
+  </v-layout>
+  <v-layout mt-4>
+    <v-flex md5>
+      <song-textarea title="Tabs" :text="song.tab" />
+    </v-flex>
+    <v-flex md6 offset-md1>
+      <song-textarea title="Lyrics" :text="song.lyrics" />
+    </v-flex>
+  </v-layout>
+  </div>
+</template>
+
+<script>
+import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+import SongMetadata from './SongMetadata'
+import SongTextarea from './SongTextarea'
+import YouTube from './YouTube'
+
+export default {
+  data () {
+    return {
+      song: {}
+    }
+  },
+  components: {
+    Panel,
+    SongMetadata,
+    SongTextarea,
+    YouTube
+  },
+  async mounted () {
+    try {
+      const songId = this.$store.state.route.params.songId
+      this.song = (await SongsService.show(songId)).data;
+    } catch(err) {
+      console.log('Error trying to find the song.')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
