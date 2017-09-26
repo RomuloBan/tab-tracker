a6e39c1e72b7d0e3010d3e4028b87c50158dcac7
romulo.banares <romulo88@hotmail.com>
2017-09-26 12:32:42 +0200

    create YouTube component

---
+++ client/src/components/ViewSong/YouTube.vue
@@ -0,0 +1,27 @@
+<template>
+  <panel title="YouTube Video">
+    <youtube
+      :video-id="youtubeId"
+      :player-width="700"
+      :player-height="300"
+      >
+    </youtube>
+  </panel>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+export default {
+  props: [
+    'youtubeId'
+  ],
+  components: {
+    Panel
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
