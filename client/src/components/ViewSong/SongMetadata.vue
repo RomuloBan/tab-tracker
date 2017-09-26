157556e785859a3a9229ae99f234eac1799fd798
romulo.banares <romulo88@hotmail.com>
2017-09-26 12:32:58 +0200

    create SongMetadata component

---
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -0,0 +1,58 @@
+<template>
+  <panel title="Song Metadata">
+    <v-layout>
+      <v-flex sx6>
+        <div class="song-title">
+          {{song.title}}
+        </div>
+        <div class="song-artist">
+          {{song.artist}}
+        </div>
+        <div class="song-genre">
+          {{song.genre}}
+        </div>
+      </v-flex>
+
+      <v-flex xs6>
+        <img class="album-image" :src="song.albumImage" />
+        <br/>
+        {{song.album}}
+      </v-flex>
+    </v-layout>
+  </panel>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+
+export default {
+  props: [
+    'song'
+  ],
+  components: {
+    Panel
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 1.1rem;
+}
+.song-artist {
+  font-size: 1rem;
+}
+.song-genre {
+  font-size: 0.9rem;
+}
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
