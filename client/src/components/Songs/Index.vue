bbe9629ee4dd8ac1f81eb87f9812593c67528015
romulo.banares <romulo88@hotmail.com>
2017-09-27 14:48:00 +0200

    remove Panel import from Index Song comp

--- client/src/components/Songs/Index.vue
+++ client/src/components/Songs/Index.vue
@@ -1,73 +1,25 @@
 <template>
   <v-layout column>
     <v-flex xs6 offset-xs3>
-        <panel title="Songs">
-          <v-btn
-          fab
-          class="cyan accent-3"
-          slot="action"
-          small
-          absolute
-          @click="navigateTo({name: 'create-song'})"
-          right>
-            <v-icon
-            >add</v-icon>
-          </v-btn>
-          <div
-            v-for="song in songs"
-            class="song"
-            :key="song.id">
-            <v-layout mb-4>
-              <v-flex xs6>
-                <div class="song-title">
-                  {{song.title}}
-                </div>
-                <div class="song-artist">
-                  {{song.artist}}
-                </div>
-                <div class="song-genre">
-                  {{song.genre}}
-                </div>
-
-                <v-btn
-                  dark
-                  class="cyan"
-                  @click="navigateTo({
-                    name: 'song',
-                    params: {
-                      songId: song.id
-                    }
-                  })">
-                  View
-                </v-btn>
-
-              </v-flex>
-               <v-flex xs6>
-                 <img class="album-image" :src="song.albumImage">
-              </v-flex>
-            </v-layout>
-          </div>
-        </panel>
+      <search-panel />
+      <songs-panel :songs="songs" class="mt-2"/>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import SongsService from '@/services/SongsService'
-import Panel from '@/components/Panel'
+import SongsPanel from './SongsPanel'
+import SearchPanel from './SongsSearchPanel'
 export default {
   data () {
     return {
-      songs: null
+      songs: {}
     }
   },
   components: {
-    Panel
-  },
-  methods: {
-    navigateTo (route) {
-      this.$router.push(route)
-    }
+    SongsPanel,
+    SearchPanel
   },
   async mounted () {
     this.songs = (await SongsService.index()).data
@@ -78,22 +30,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.song {
-  padding: 20px;
-  height: 330px;
-  overflow: hidden;
-}
-.song-title {
-  font-size: 1.1rem;
-}
-.song-artist {
-  font-size: 1rem;
-}
-.song-genre {
-  font-size: 0.9rem;
-}
-.album-image {
-  width: 70%;
-  margin: 0 auto;
-}
+
 </style>
