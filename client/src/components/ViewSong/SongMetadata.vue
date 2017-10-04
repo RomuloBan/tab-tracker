a9e14b8423341892aa91fb6a83890bcb8e503408
romulo.banares <romulo88@hotmail.com>
2017-10-04 13:33:23 +0200

    add mapState to songMetadata comp

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -66,7 +66,8 @@ export default {
   },
   computed: {
     ...mapState([
-      'isUserLoggedIn'
+      'isUserLoggedIn',
+      'user'
     ])
   },
   watch: {
@@ -75,10 +76,13 @@ export default {
         return
       }
       try {
-        this.bookmark = (await BookmarksService.index({
+        const bookmarks = (await BookmarksService.index({
           songId: this.song.id,
-          userId: this.$store.state.user.id
+          userId: this.user.id
         })).data
+        if(bookmarks.length) {
+          this.bookmark = bookmarks[0]
+        }
       } catch (err) {
         console.log("Error trying to get bookmarks")
       }
@@ -89,7 +93,7 @@ export default {
       try {
         this.bookmark = (await BookmarksService.post({
           songId: this.song.id,
-          userId: this.$store.state.user.id
+          userId: this.user.id
         })).data
       } catch (err) {
         console.log("Error trying to add a bookmark")
