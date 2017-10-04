25edefc67d01c6ebec8932d856eff5f3157af6e8
romulo.banares <romulo88@hotmail.com>
2017-10-04 13:32:14 +0200

    create SongsBookmarks component

---
+++ client/src/components/Songs/SongsBookmarks.vue
@@ -0,0 +1,64 @@
+<template>
+  <panel title="Bookmarks">
+    <v-data-table
+      :headers="headers"
+      :items="bookmarks"
+      :pagination.sync="pagination"
+      class="elevation-1"
+    >
+      <template slot="items" scope="props">
+        <td  class="text-xs-right">{{ props.item.title }}</td>
+        <td  class="text-xs-right">{{ props.item.artist }}</td>
+      </template>
+    </v-data-table>
+  </panel>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import BookmarksService from '@/services/BookmarksService'
+export default {
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Title',
+          value: 'title'
+        },
+        {
+          text: 'Artist',
+          value: 'artist'
+        }
+      ],
+      bookmarks: [],
+      pagination: {
+        sortBy: 'date',
+        descending: true
+      }
+    }
+  },
+    computed: {
+    ...mapState([
+      'isUserLoggedIn',
+      'user'
+    ])
+  },
+  async mounted () {
+    if(this.isUserLoggedIn) {
+      try {
+        this.bookmarks = (await BookmarksService.index({
+          userId: this.user.id
+        })).data
+      } catch(err) {
+        console.log(err)
+      }
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
