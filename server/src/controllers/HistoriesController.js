8f49a30538cfb4389c521a1b40388f703587f02b
romulo.banares <romulo88@hotmail.com>
2017-10-05 15:42:35 +0200

    create HistoriesController

---
+++ server/src/controllers/HistoriesController.js
@@ -0,0 +1,48 @@
+const {History, Song} = require('../models')
+const _ = require('lodash')
+
+module.exports = {
+
+  async index (req, res) {
+    try {
+      const { userId } = req.query
+      const histories = await History.findAll({
+        where: {
+          UserId: userId
+        },
+        distinct: 'UserId',
+        include: [
+          {
+            model: Song
+          }
+        ]
+      })
+        .map(historyItem => historyItem.toJSON())
+        .map(historyItem => Object.assign(
+          {},
+          historyItem,
+          historyItem.Song
+        ))
+      res.send(_.uniqBy(histories, history => history.SongId))
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to get the history.'
+      })
+    }
+  },
+
+  async post (req, res) {
+    try {
+      const { songId, userId } = req.body
+      const history = await History.create({
+        SongId: songId,
+        UserId: userId
+      })
+      res.send(history)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error occured trying to create history object'
+      })
+    }
+  }
+}
