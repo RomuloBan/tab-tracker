0e005ab562621cf95f7962366291b3197b655558
romulo.banares <romulo88@hotmail.com>
2017-10-10 21:40:04 +0200

    remove params from Api get

--- client/src/services/HistoriesService.js
+++ client/src/services/HistoriesService.js
@@ -2,9 +2,7 @@ import Api from '@/services/Api'
 
 export default {
   index (params) {
-    return Api().get('histories', {
-      params: params
-    })
+    return Api().get('histories')
   },
   post (history) {
     return Api().post('histories', history)
